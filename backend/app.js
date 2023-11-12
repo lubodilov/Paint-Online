3cf5ad15ef6fd170cce06196c9c1e346c19b3274
SnowtromAux <snowtromgs@gmail.com>
2023-11-12 23:55:09 +0200

    init (2 / 2)

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,11 @@
 const express = require('express');
-const path = require('path');
 const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
+const path = require('path');
 require('dotenv').config();
 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -10,7 +14,15 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views/home.html'));
 });
 
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    // socket.on('disconnect', () => {
+    //     console.log('User disconnected');
+    // });
+});
+
 const PORT = process.env.PORT || 8000;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
