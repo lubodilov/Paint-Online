8b0ffa7f5b54d8c676fe5f8d68fc8e41fdd5da40
SnowtromAux <snowtromgs@gmail.com>
2023-11-12 23:35:17 +0200

    init (1 / 2)

---
+++ backend/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+require('dotenv').config();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/home.html'));
+});
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
