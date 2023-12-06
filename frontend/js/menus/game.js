9c356ab2bcbf597d3237a1bf5388c36eec4e6ec3
lubodilov <46284108+lubodilov@users.noreply.github.com>
2023-12-06 04:47:34 +0200

    80% done

--- frontend/js/menus/game.js
+++ frontend/js/menus/game.js
@@ -6,15 +6,33 @@ const player_data = storage.getPlayerData();
 const party_data = storage.getPartyData();
 const canvas = document.getElementById("gameCanvas");
 
-player = new Player(player_data.id , player_data.name , player_data.image);
-player.party = new Party(party_data.code ,
-                         party_data.creator_id, 
-                         party_data.creator_name,
-                         party_data.player_ids,
-                         party_data.player_names, 
-                         party_data.max_players,
-                         player);
-game = new Game(player , canvas);
+player = new Player(player_data.id, player_data.name, player_data.image);
+player.party = new Party(
+  party_data.code,
+  party_data.creator_id,
+  party_data.creator_name,
+  party_data.player_ids,
+  party_data.player_names,
+  party_data.max_players,
+  player
+);
+game = new Game(player, canvas);
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Get the dropdown button element
+  var dropdownBtn = document.querySelector("#basic-tools");
+
+  // Toggle the display of the dropdown content on click
+  dropdownBtn.addEventListener("click", function () {
+    this.classList.toggle("show");
+    var dropdownContent = this.querySelector("div");
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  });
+});
 
 // game.listenForSockets();
 game.listenForEvents();
