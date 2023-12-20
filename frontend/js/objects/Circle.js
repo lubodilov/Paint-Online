ef5adc64c89302cf780b3442f6d156a5fbd38cc6
SnowtromAux <snowtromgs@gmail.com>
2023-12-20 06:06:30 +0200

    mini fix

--- frontend/js/objects/Circle.js
+++ frontend/js/objects/Circle.js
@@ -14,7 +14,7 @@ class Circle {
   }
 
   startCircle(fromX, fromY) {
-    this.corner_coords = { x: fromX, y: fromY };
+    this.center_coords = { x: fromX, y: fromY };
     this.ctx.putImageData(this.image_data, 0, 0);
 
     this.ctx.beginPath();
@@ -32,11 +32,11 @@ class Circle {
     this.ctx.putImageData(this.image_data, 0, 0);
     this.ctx.beginPath();
 
-    const centerX = (this.corner_coords.x + toX) / 2;
-    const centerY = (this.corner_coords.y + toY) / 2;
+    const centerX = (this.center_coords.x + toX) / 2;
+    const centerY = (this.center_coords.y + toY) / 2;
     this.radius = Math.sqrt((toX - centerX) ** 2 + (toY - centerY) ** 2);
 
-    this.ctx.arc(centerX, centerY, this.radius, 0, 2 * Math.PI);
+    this.ctx.arc(this.center_coords.x , this.center_coords.y, this.radius, 0, 2 * Math.PI);
     this.ctx.stroke();
   }
 
