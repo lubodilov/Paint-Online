41b0ae3ba80a7d6940e77c61d8928686a180a397
SnowtromAux <snowtromgs@gmail.com>
2024-02-17 12:35:58 +0200

    bug fix

--- frontend/js/managers/selectManager.js
+++ frontend/js/managers/selectManager.js
@@ -1,17 +1,23 @@
 function startSelect(c, ev) {
+
     if (c.select_obj) return;
     
+    console.log("start");
+
     const image_data = c.ctx.getImageData(0, 0, c.canvas_width, c.canvas_height);
+
     c.cur_figure = new Select(image_data);
     c.cur_figure.startSelect(ev.offsetX, ev.offsetY);
   
     // c.updateCanvas(c.cur_canvas_data);
-    socket.emit("create-figure", player.party.code, c.cur_canvas_data);
+    // socket.emit("create-figure", player.party.code, c.cur_canvas_data);
   }
   
   function updateSelect(c, ev) {
     if (!c.cur_figure || c.select_obj) return;
 
+    console.log("update");
+ 
     c.ctx.clearRect(0, 0, c.canvas_width, c.canvas_height);
     c.cur_figure.updateSelect(ev.offsetX, ev.offsetY);
   
@@ -22,17 +28,29 @@ function startSelect(c, ev) {
   function finishSelect(c, ev) {
     if (!c.cur_figure || c.select_obj) return;
 
+    console.log("finish");
+
     c.cur_figure.finishSelect(ev.offsetX, ev.offsetY);
   
-    c.finishFigure(c.cur_canvas_data);
+    // c.finishFigure(c.cur_canvas_data);
     // socket.emit("finish-figure", player.party.code, c.cur_canvas_data);
 
+    c.finished = true;
     c.select_obj = c.cur_figure;
+    console.log(c.cur_figure)
     c.cur_figure = undefined;
   }
 
   function toggleSelect(c , ev){
+    console.log(c.select_obj)
     if(!c.select_obj)return;
+    if(c.finished){
+      c.finished = false;
+      return;
+    }
+
+    console.log("click");
+
     const cur_figure = c.select_obj;
 
     if(!(ev.offsetX >= Math.min(cur_figure.from_coords.x , cur_figure.bottom_coords.x)
@@ -42,7 +60,9 @@ function startSelect(c, ev) {
 {
 
     c.ctx.clearRect(0 , 0 , c.canvas_width , c.canvas_height);
-    c.updateCanvas(cur_figure.image_data)
+
+    c.ctx.putImageData(cur_figure.image_data, 0, 0);
+    c.updateCanvas(cur_figure.image_data);
 
     c.select_obj = undefined;
 }
